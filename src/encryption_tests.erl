e7dc876ccd45c3ae55cf24cafe3bda4cbe96f2ca
Robin Larsson <gwaerondor@gmail.com>
2017-09-23 21:28:20 +0200

    Removed unused test module

--- src/encryption_tests.erl
+++
@@ -1,9 +0,0 @@
--module(encryption_tests).
--include_lib("eunit/include/eunit.hrl").
-
-both_modules_should_encrypt_the_same_test() ->
-    Input = "This is my string",
-    Seed = 1337,
-    Result_1 = ericsson:encrypt(Input, Seed),
-    Result_2 = enc:encrypt(Input, Seed),
-    ?assertEqual(Result_1, Result_2).
